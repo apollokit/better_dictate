4e212a2d4c795f56ea3f981c04c6c4c7f7d9dc1c
Kit Kennedy <kitkennedy8@gmail.com>
2019-11-10 16:28:18 -0800

    Updating setup for basic deepspeech library usage

--- setup.sh
+++ setup.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+# see https://github.com/mozilla/DeepSpeech/releases/tag/v0.5.1 for more context
+
 # install reqs for pyaudio
 sudo apt install portaudio19-dev
+
+# get the deepspeech language model
+curl -LO https://github.com/mozilla/DeepSpeech/releases/download/v0.5.1/deepspeech-0.5.1-models.tar.gz
+tar xvf deepspeech-0.5.1-models.tar.gz
+
+./create_virtualenv.sh
