707defd56d7d435203f1149a22e894ff5bb2149b
Kit Kennedy <kitkennedy8@gmail.com>
2020-11-08 18:44:09 -0800

    Updating gitignore and reorg of env files

--- env/create_virtualenv.sh
+++ env/create_virtualenv.sh
@@ -4,7 +4,7 @@ virtualenv_dir=$HOME/.config/virtualenvs/bdict_venv
 if [ -e "$virtualenv_dir" ]; then
 	echo "$virtualenv_dir already exists. We'll just update requirements."
 else
-	virtualenv -p python3  $virtualenv_dir
+	virtualenv -p python3.8  $virtualenv_dir
 	./setup.sh
 fi
 
