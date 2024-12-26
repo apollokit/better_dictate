b552349f44716232e3d8cebd5029bd4cd5a55868
Kit Kennedy <kitkennedy8@gmail.com>
2024-12-26 14:30:28 -0600

    Updating code to work on macos 15.1.1

--- env/create_virtualenv.sh
+++ env/create_virtualenv.sh
@@ -4,7 +4,7 @@ virtualenv_dir=$HOME/.config/virtualenvs/bdict_venv
 if [ -e "$virtualenv_dir" ]; then
 	echo "$virtualenv_dir already exists. We'll just update requirements."
 else
-	virtualenv -p python3.8  $virtualenv_dir
+	python3.11 -m venv $virtualenv_dir
 fi
 
 source source_virtualenv
