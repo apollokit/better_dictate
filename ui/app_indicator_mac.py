6e905d817537543a8d22a7fbf506a94f263a87fd
Kit Kennedy <kitkennedy8@gmail.com>
2025-10-16 17:27:02 -0700

    add a sleep/wake menu command

--- ui/app_indicator_mac.py
+++ ui/app_indicator_mac.py
@@ -7,6 +7,7 @@ from multiprocessing import Process, Queue
 from os import path
 import time
 from threading import Thread
+from queue import Empty as QueueEmpty  # for non-blocking reads
 
 from PyQt5.QtGui import QIcon
 from PyQt5.QtCore import QThread
@@ -58,8 +59,13 @@ class UpdaterThread(QThread):
             else: 
                 raise NotImplementedError
             
+# event names
+EV_TOGGLE_SLEEP = "toggle_sleep"
+EV_UI_QUIT = "ui_quit"
 
-def menu_bar_job(menu_command_to_queue: Queue):
+# --- menu process ---
+
+def menu_bar_job(menu_command_to_queue: Queue, menu_event_from_ui: Queue):
     logger.info('Menu bar job started')
 
     app = QApplication([])
@@ -77,28 +83,46 @@ def menu_bar_job(menu_command_to_queue: Queue):
     # Create the menu
     menu = QMenu()
 
+
+    # Add a sleep/wake option to the menu.
+    switch_state = QAction("Sleep/Wake")
+    # On click, emit an event. Your manager decides what to do.
+    switch_state.triggered.connect(lambda _checked=False: menu_event_from_ui.put(EV_TOGGLE_SLEEP))
+    menu.addAction(switch_state)
+
+    # add an empty action just to avoid accidentally hitting quit too much
+    empty_action = QAction("-")
+    menu.addAction(empty_action)
+
     # Add a Quit option to the menu.
-    quit = QAction("Quit")
-    quit.triggered.connect(app.quit)
-    menu.addAction(quit)
+    quit_action = QAction("Quit")
+    # Instead of closing the app directly, emit an event up to the manager.
+    quit_action.triggered.connect(lambda: menu_event_from_ui.put(EV_UI_QUIT))
+    menu.addAction(quit_action)
 
     # Add the menu to the tray
     tray.setContextMenu(menu)
 
-    updater_thread = UpdaterThread(menu_command_to_queue, 
-        app, tray, active_icon, sleeping_icon)
+    updater_thread = UpdaterThread(menu_command_to_queue, app, tray, active_icon, sleeping_icon)
     updater_thread.start()
 
     app.exec_()
 
+# --- manager side ---
+
 class MenuBarManager:
     def __init__(self):
-        # queue to send commands to the menu bar proc
+        # queue manager -> menu (icon updates, quit)
         self._menu_command_to_queue = Queue()
 
-        self._menu_bar_proc = Process(target=menu_bar_job, 
-            args=(self._menu_command_to_queue,))
-        self._manager_thread = Thread(target=self.do_manage)
+        # queue menu -> manager (user clicks)
+        self._menu_event_from_ui = Queue()
+
+        self._menu_bar_proc = Process(
+            target=menu_bar_job,
+            args=(self._menu_command_to_queue, self._menu_event_from_ui),
+        )
+        self._manager_thread = Thread(target=self.do_manage, daemon=True)
 
         self._last_app_mngr_state_sleep = True
 
@@ -106,7 +130,6 @@ class MenuBarManager:
         """Start the process. Need to call terminate() at some point too 
         """
         self._menu_bar_proc.start()
-
         self._manager_thread.start()
 
     def terminate(self):
@@ -117,6 +140,7 @@ class MenuBarManager:
         # end the proc
         # todo make sure queue is clear, per https://docs.python.org/3/library/multiprocessing.html#programming-guidelines
         # todo throw error if already joined
+        self._menu_command_to_queue.put('quit')
         self._menu_bar_proc.join()
         self._manager_thread.join()
         logger.info('MenuBarManager process and thread terminated')
@@ -131,19 +155,33 @@ class MenuBarManager:
                 self._menu_command_to_queue.put('quit')
                 break
 
-            # set the app icon depending on whether we're sleeping or not
+            # Handle UI -> manager events without blocking
+            try:
+                while True:
+                    ev = self._menu_event_from_ui.get_nowait()
+                    if ev == EV_TOGGLE_SLEEP:
+                        app_mngr.toggle_sleep()
+                    elif ev == EV_UI_QUIT:
+                        app_mngr.signal_quit()
+                        self._menu_command_to_queue.put('quit')
+                        return
+                    else:
+                        logger.warning(f"Unknown menu event: {ev}")
+            except QueueEmpty:
+                pass
+
+            # Reflect current app state in tray icon
             if app_mngr.sleeping:
                 if not self._last_app_mngr_state_sleep:
                     self._menu_command_to_queue.put('sleep')
             else:
                 if self._last_app_mngr_state_sleep:
                     self._menu_command_to_queue.put('active')
-            
             self._last_app_mngr_state_sleep = app_mngr.sleeping
 
-            # the user has selected to quite from the UI, so kill the overall app
+            # Fallback: if tray proc died unexpectedly, shut down app
             if not self._menu_bar_proc.is_alive():
                 app_mngr.signal_quit()
+                break
 
             time.sleep(0.01)
-
